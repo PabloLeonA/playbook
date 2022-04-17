c529803d28db1f3e09eb3bb38fd6d47a1c35132f
PabloLeonA <jpablo.lea@gmail.com>
2022-04-17 17:44:25 -0500

    agregando objeto con varias propiedades

---
+++ weekly_mission_2/example1/example1_3.js
@@ -0,0 +1,21 @@
+console.log("creacion de objeto con varias propiedades")
+
+const Myobject3 = {
+    name:"Fernando",
+    age:27,
+    nickname:[
+        "Fer",
+        "Fercho",
+        "Fern"
+    ],
+    adress:{
+        zip_code: "22793",
+        street: "Felipe cordoba 24 av la paz",
+        city:"españa"
+        }
+    }
+    console.log("Ejemplo3:objeto con varias propiedades")
+    console.log(Myobject3)
+    console.log(Myobject3.name)
+    console.log(Myobject3["adress"])
+
