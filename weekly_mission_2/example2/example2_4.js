9fe85b07587b6bd8048df80b704195c4834a3598
PabloLeonA <jpablo.lea@gmail.com>
2022-04-17 22:04:20 -0500

    agregando map para reccorrer una lista y crear una nueva

---
+++ weekly_mission_2/example2/example2_4.js
@@ -0,0 +1,6 @@
+console.log("Uso de map para recorrer elementos de una lista y crear una nueva lista")
+
+const number4 =[1,2,3,4,5]
+const numbersSquare = number4.map(function(num){ return num*num})
+console.log("Ejemplo 4: imprimiendo la lista de elementos al cuadrado")
+console.log(numbersSquare)
