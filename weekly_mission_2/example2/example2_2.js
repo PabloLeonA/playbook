cbfba5d32bc9afd3b2251f35f10b89f14ad99574
PabloLeonA <jpablo.lea@gmail.com>
2022-04-17 20:42:41 -0500

    se agrega suma con foreach de lista de numeros

---
+++ weekly_mission_2/example2/example2_2.js
@@ -0,0 +1,7 @@
+console.log("Creacion de Each para calcular la suma de todos los elementos de una lista")
+
+let sum = 0;
+const number2= [1,2,3,4,5,];
+number2.forEach(num => sum +=num)
+console.log ("Ejemplo 2 :calculo de elementos de una list")
+console.log(sum)
