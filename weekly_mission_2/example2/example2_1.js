68de56f2a178f19a774eb1f77ac4608adf916fbf
PabloLeonA <jpablo.lea@gmail.com>
2022-04-17 20:11:14 -0500

    agregando each para imprimir lista de elemntos

---
+++ weekly_mission_2/example2/example2_1.js
@@ -0,0 +1,5 @@
+console.log("creacion de for each para imprimir elementos de una lista")
+
+const number =[1,2,3,4,5]
+console.log("Ejemplo 1 imprimiendo numeros de lista")
+number.forEach(num => console.log(num))
