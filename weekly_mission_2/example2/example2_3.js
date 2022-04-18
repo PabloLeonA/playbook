d80fbac70e53e969f75e63b4fd79a79ca84077ad
PabloLeonA <jpablo.lea@gmail.com>
2022-04-17 20:45:05 -0500

    se agrega foreach para imprimir paises en mayusculas

---
+++ weekly_mission_2/example2/example2_3.js
@@ -0,0 +1,5 @@
+console.log ("creacion de for each para imprimir paises en letras mayusculas")
+
+const contries = ['Finland','Denmark','Swenden','Norway','iceland']
+console.log("Ejemplo 3 imprimiendo paises en mayuscula")
+contries.forEach((element) => console.log (element.toUpperCase()))
