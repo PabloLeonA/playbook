144ae9e515169cb189816c223ef6cc6fc10343e1
PabloLeonA <jpablo.lea@gmail.com>
2022-04-17 22:18:39 -0500

    se agrega ejemplo de map para convertir nombres de una lista a minusculas

---
+++ weekly_mission_2/example2/example2_5.js
@@ -0,0 +1,7 @@
+console.log ("creacion de map para convertir los nombres de una lista a minusculas")
+
+const names =['Roberto','Fernando','Cristian','Ash']
+const namesToUpperCase= names.map((name) => name.toUpperCase())
+
+console.log("Ejemplo 5: uso de map para convertir los nomrbes de una,lista a minusculas")
+console.log(namesToUpperCase)
