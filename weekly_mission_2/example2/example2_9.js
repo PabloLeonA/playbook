3ac08ec3acb58569201524838611ec35f6c3afaa
PabloLeonA <jpablo.lea@gmail.com>
2022-04-19 21:33:31 -0500

    se agrega  proceso uso de reduce

---
+++ weekly_mission_2/example2/example2_9.js
@@ -0,0 +1,6 @@
+console.log("Creacion de uso de reduce")
+
+const numbers9 = [1,2,3,4,5,]
+const result_of_reduce = numbers9.reduce((acc,element) => acc + element,0)
+console.log("Ejemplo 9 : uso de reduce para calcular la suma de elementos de una lista")
+console.log(result_of_reduce)
