00f5b65fabda941fdd0cfe175d9b6cbadbb689ba
PabloLeonA <jpablo.lea@gmail.com>
2022-05-05 23:31:34 -0500

    Uso de sort para ordenar elementos

---
+++ weekly_mission_2/example2/example2_15.js
@@ -0,0 +1,4 @@
+console.log("Uso de sort para ordenar elementos") 
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
+console.log("Ejemplo 15: Elementos ordernados usando SORT")
+console.log(products.sort())
