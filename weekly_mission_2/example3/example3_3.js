50bd8e7613ac90615c50d86753e447af1d8af905
PabloLeonA <jpablo.lea@gmail.com>
2022-05-10 00:13:22 -0500

    se agrega ejemplo de instanciar objeto con atributos

---
+++ weekly_mission_2/example3/example3_3.js
@@ -0,0 +1,11 @@
+console.log("Ejemplo 3 instanciar un objeto con atributos")
+
+class student{
+//el constructor nos permite instanciar un objeto y asignarle atributos(this nos ayuda a realizar esta funcion)
+constructor(name,age,subjects){
+    this.name = name
+    this.age = age
+    this.subjects =subjects
+} 
+
+}
