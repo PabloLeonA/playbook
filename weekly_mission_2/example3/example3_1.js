78a76d0a145890b06cc897473b85664f2ae9ed92
PabloLeonA <jpablo.lea@gmail.com>
2022-05-09 23:52:16 -0500

    se agrega clase vacia

---
+++ weekly_mission_2/example3/example3_1.js
@@ -0,0 +1,7 @@
+console.log("Ejemplo 1 :creacion de clase vacia")
+
+class person{
+//clase sin contenido
+}
+console.log("Ejemplo de clase vacia")
+console.log(person) //[class person llamado de clase]
