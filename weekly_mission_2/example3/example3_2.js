343b677943770ee08292d2c2dbf87589f6f40f7f
PabloLeonA <jpablo.lea@gmail.com>
2022-05-10 00:02:26 -0500

    se agrega creacion de objeto apartir de una clase

---
+++ weekly_mission_2/example3/example3_2.js
@@ -0,0 +1,7 @@
+console.log("creacion de un objeto apartir de una clase")
+
+class pet {
+}
+const mypet1 = new pet() // se pueden crear muchos objetos de la clase pet
+console.log("Ejemplo de creacion de un objeto apartir de una clase")
+console.log(mypet1) // objeto de la clase pet
